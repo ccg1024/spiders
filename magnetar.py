112119a5100fb676a7efa8cbb7556ed14103630d
ccg1024 <aimerchenyuan@163.com>
2022-03-05 16:18:30 +0800

    formate code

--- magnetar.py
+++ magnetar.py
@@ -1,40 +1,49 @@
 # -*- coding: utf-8 -*-
 """
- 文件描述:
- 终端显示方式
+ File description:
+ The display format of terminal.
+
  [+] MAGNET SPIDER
  [>] INTO SCRIPT CONSOLE!
- [>] INPUT KEY WORDS:
- [INFO] 2022-02-22 xxx
+ [<] INPUT KEY WORDS:
  [#] RESULTS
  # [ 1] xx
  # [ 2] xx
+ # .... xx
  # [12] xx
- [>] OPTION WORDS: [n] next;
- [$]:
+ [>] OPTION WORDS: [n] next; page:1/?; [r] search new; [e] exit;
+ [<] OPTION WORDS: 
  [>] BEGIN NEW SEARCH
- [>] INPUT KEY WORDS:
- @作者: ccg
- @日期: 2022 02 09 3:19 下午
- @路径: pythonProject-PyCharm-magnetar
+ [<] INPUT KEY WORDS:
+
+ @autor: ccg
+ @date: 2022 02 09 3:19 下午
+ @path: pythonProject-PyCharm-magnetar
+
 """
 import spider
-import json
 import argparse
 
+
+# add command
 parser = argparse.ArgumentParser(description='crawl magnet through api: https://api.magnetar.cc')
 parser.add_argument('-v', '--version', help='show the script version', action='store_true')
 parser.add_argument('-w', '--word', help='search word, when run the script')
-parser.add_argument('-t', '--type', help='the type of source shown, all; video; img; compress; music;'
-                                         'program; document; other', choices=['all', 'video', 'img', 'compress',
-                                                                              'music', 'program', 'document',
-                                                                              'other'])
-parser.add_argument('-s', '--safe', help='whether open safe search, when close, the result may include some porn '
-                                         'message', action='store_true')
-parser.add_argument('-o', '--order', help='choose how to sort the resources, default; time; size; repeat',
-                    choices=['default', 'time', 'size', 'repeat'])
+parser.add_argument('-t', '--type', help='the type of source shown, all; video; img; compress;'
+                                         'music; program; document; other', choices=['all',
+                                                                                     'video', 'img',
+                                                                                     'compress',
+                                                                                     'music',
+                                                                                     'program',
+                                                                                     'document',
+                                                                                     'other'])
+parser.add_argument('-s', '--safe', help='whether open safe search, when close, the result may '
+                                         'include some porn message', action='store_true')
+parser.add_argument('-o', '--order', help='choose how to sort the resources, default; time; size; '
+                                          'repeat', choices=['default', 'time', 'size', 'repeat'])
 args = parser.parse_args()
 
+# information aboult scirpt
 info = """[+] MAGNET SPIDER"""
 version = '1.0.0'
 
@@ -42,15 +51,20 @@ version = '1.0.0'
 magnet_head = 'magnet:?xt=urn:btih:'
 
 
+# crawler api
 def api_func(key_words, crawl, page=1, file_order=None, file_type=None):
-    # m 代表排序方式，t 代表类型
+    # m stands for sorting, t stands for type
     file_order = str(file_order) if file_order is not None else 'time'
     file_type = str(file_type) if file_type is not None else 'video'
-    url = f'https://api.magnetar.cc/search/v2.index/search?q={key_words}&m={file_order}&t={file_type}&p={page}'
-    # 得到请求结果
+    url = f'https://api.magnetar.cc/search/v2.index/search?q={key_words}&m={file_order}&' \
+          f't={file_type}&p={page}'
+ 
+    # obtains response source
     api_result = crawl.get(url).text
-    json_result = json.loads(api_result)
+    # json_result = json.loads(api_result)
 
+    # search_btdatas = json_result['searchBtData']
+    json_result = crawl.json_crawl(api_result)
     search_btdatas = json_result['searchBtData']
 
     total_data = 0
@@ -59,15 +73,16 @@ def api_func(key_words, crawl, page=1, file_order=None, file_type=None):
     if search_btdatas is None:
         print(f'can not find anything form API for: {key_words}')
     else:
-        # 资源总数
+        # total number of data
         total_data = json_result['total']
-        
+
         item_num = 1
         print('[#] RESULTS')
         for btdata in search_btdatas:
             print('# [{:2d}]'.format(item_num), end=' ')
-            print(btdata['name'].replace('<font style=\'color:#dd4b39;\'>', '').replace('</font>', ''))
-            
+            print(btdata['name'].replace('<font style=\'color:#dd4b39;\'>', '')
+                  .replace('</font>', ''))
+
             print('# \t\t ', end='')
             print(magnet_head + btdata['hash'])
             item_num += 1
@@ -75,23 +90,22 @@ def api_func(key_words, crawl, page=1, file_order=None, file_type=None):
     return int(total_data), item_num - 1
 
 
+# display input information
 def input_control(first_word):
     """
-    输入控制器
-    :return:
+    controllor of input
     """
     if first_word is not None:
         key_words = first_word
         print(f'[>] INPUT KEY WORDS: {key_words}')
     else:
-        key_words = input('[>] INPUT KEY WORDS: ')
+        key_words = input('[<] INPUT KEY WORDS: ')
     return key_words
 
 
 def option_control(page_num, total_page):
     """
-    选项控制器
-    :return:
+    controllor of options
     """
     # option control
     console_options = "page:{}/{};  [r] search new;  [e] exit;".format(page_num, total_page)
@@ -105,13 +119,12 @@ def option_control(page_num, total_page):
 
 def get_console_token(PRE_PAGE, NEXT_PAGE, EXIT_CONSOLE, NEW_SEARCH):
     """
-    获取脚本行为命令
-    :return:
+    obtains script's command
     """
-    # 获取控制器行为
+    # Get controller behavior
     console_token = input("[<] OPTION WORDS: ")
 
-    # 判断控制器命令格式
+    # Determine the controller command format
     while console_token not in (PRE_PAGE, NEXT_PAGE, EXIT_CONSOLE, NEW_SEARCH):
         print("[INFO] The key world should in ({}, {}, {}, {})".format(PRE_PAGE, NEXT_PAGE,
                                                                        EXIT_CONSOLE, NEW_SEARCH))
@@ -122,53 +135,52 @@ def get_console_token(PRE_PAGE, NEXT_PAGE, EXIT_CONSOLE, NEW_SEARCH):
 
 def main():
     """
-    控制台方法
+    Controllor function
     """
     NEXT_PAGE = 'n'
     PRE_PAGE = 'N'
     EXIT_CONSOLE = 'e'
     NEW_SEARCH = 'r'
 
-    # 获取命令行参数
+    # Get args of command line
     file_order = args.order
     file_type = args.type
     safe_search = args.safe
     first_word = args.word
 
-    # 创建爬虫对象
+    # Create crawler object
     crawl = spider.Crawl()
     # crawl = Crawl()
 
-    # 添加请求头元素
+    # Add key-value pairs of request header
     tag_safe = 'on' if safe_search else 'off'
     crawl.add_headers_elem('safesearch', tag_safe)
 
     print(info)
     print("[>] INTO SCRIPT CONSOLE!")
-    # 控制台输入
+
+    # controller input
     console_token = NEW_SEARCH
     total_page = 0
     page_num = 1
     key_words = ''
     while console_token != EXIT_CONSOLE:
         if console_token == NEW_SEARCH:
-            # 封装进了函数
-            # if first_word is not None:
-            #     key_words = first_word
-            #     first_word = None
-            #     print(f'[>] INPUT KEY WORDS: {key_words}')
-            # else:
-            #     key_words = input('[>] INPUT KEY WORDS: ')
             key_words = input_control(first_word)
+
+            # Determine whether key_words is an exit command
+            if key_words.replace(' ', '') == 'exit' or key_words.replace(' ', '') == 'e':
+                break
+
             first_word = None
             page_num = 1
             try:
-                total_data, page_items = api_func(key_words, crawl, page=page_num, file_order=file_order,
-                                                  file_type=file_type)
-                
+                total_data, page_items = api_func(key_words, crawl, page=page_num,
+                                                  file_order=file_order, file_type=file_type)
+
                 if total_data == 0:
                     continue
-                    
+
                 total_page = total_data // page_items
                 if total_data % page_items != 0:
                     total_page += 1
@@ -176,23 +188,8 @@ def main():
                 print('[ERROR] Got some error in \'input key words\' part.')
                 print(e)
                 break
-        # # option control --- 封装进来函数
-        # console_options = "page:{}/{};  [r] search new;  [e] exit;".format(page_num, total_page)
-        # if page_num < total_page:
-        #     console_options = '[n] next;  ' + console_options
-        # if page_num > 1:
-        #     console_options = '[N] pre;  ' + console_options
-        # print('[>] OPTION WORDS: ' + console_options)
         console_options = option_control(page_num=page_num, total_page=total_page)
 
-        # # 获取控制器行为  --- 封装进函数
-        # console_token = input("[<] OPTION WORDS: ")
-        #
-        # # 判断控制器命令格式
-        # while console_token not in (PRE_PAGE, NEXT_PAGE, EXIT_CONSOLE, NEW_SEARCH):
-        #     print("[INFO] The key world should in ({}, {}, {}, {})".format(PRE_PAGE, NEXT_PAGE,
-        #                                                                    EXIT_CONSOLE, NEW_SEARCH))
-        #     console_token = input("[$]:")
         console_token = get_console_token(PRE_PAGE, NEXT_PAGE, EXIT_CONSOLE, NEW_SEARCH)
 
         # make action
@@ -213,7 +210,7 @@ def main():
             print('[>] OPTION WORDS: ' + console_options)
         pass
 
-    print('[<] CLOSE.')
+    print('[>] CLOSE.')
 
 
 if __name__ == '__main__':
--- spider.py
+++ spider.py
@@ -1,22 +1,21 @@
 """
-Encapsulate some of your own commonly used crawler code.
+  Encapsulate some of your own commonly used crawler code.
+  
+  EXAMPLE
+  -------
+  >>> crawl = Crawl()
+  >>> crawl.get('https://www.baidu.com')
 
-EXAMPLE
--------
->>> crawl = Crawl()
->>> crawl.get('https://www.baidu.com')
 """
-import base64
 import json
 import os
 from typing import Any
 import requests
 from lxml import etree
 from requests.exceptions import RequestException
-from requests.models import stream_decode_response_unicode
 
 
-class Crawl():
+class Crawl:
     def __init__(self):
         self.session = requests.Session()
         self.headers = {
@@ -29,7 +28,10 @@ class Crawl():
         """
         The default headers only have user-agent key-value pairs.
 
-        :return: Returns the headers object in the crawler object
+        RETURN:
+
+        Returns the headers object in the crawler object
+
         """
         return self.headers
 
@@ -37,8 +39,11 @@ class Crawl():
         """
         Add headers key-value pairs.
 
-        :param key:   string object
-        :param value: string object
+        ARGS:
+
+        param key   : string object
+        param value : string object
+
         """
         self.headers[key] = value
         print(f'[I] Add elem \'{key}\':\'{value}\'')
@@ -47,7 +52,10 @@ class Crawl():
         """
         Modify the headers object.
 
-        :param headers: new request headers
+        ARGS:
+
+        param headers: new request headers
+
         """
         self.headers = headers
         print('[I] Replace headers successfully.')
@@ -55,6 +63,7 @@ class Crawl():
     def make_new_session(self):
         """
         Close the current session.
+
         """
         self.session.close()
         print('[I] Create a new session object')
@@ -63,9 +72,15 @@ class Crawl():
         """
         Get function of requests module.
 
-        :param url:    website of resources
-        :param kwargs: the others key-value pairs of get function
-        :return:       response
+        ARGS:
+
+        param url    : website of resources
+        param kwargs : the others key-value pairs of get function
+
+        RETURN:
+
+        response
+
         """
         try:
             res = requests.get(url, headers=self.headers, **kwargs)
@@ -79,7 +94,10 @@ class Crawl():
         """
         Post function of requests module.
 
-        :return: response
+        RETURN:
+
+        response
+
         """
         try:
             res = requests.post(url=url, headers=self.headers, **kwargs)
@@ -93,32 +111,34 @@ class Crawl():
     def _xpath(doc: Any, xpath: str = None):
         """ Perform xpath analysis on the acquired data. """
         assert doc is not None, 'the param \'doc\' should not be None.'
-        assert isinstance(doc, [str, bytes, etree._Element]), '[W] Can not recognize the type of param \'doc\''
+        assert isinstance(doc, [str, bytes]), '[W] Can not recognize the type of param \'doc\''
         assert isinstance(xpath, str), 'the param \'xpath\' should be str.'
-        
+
         if isinstance(doc, str):
             etree_obj = etree.fromstring(doc)
         elif isinstance(doc, bytes):
             etree_obj = etree.HTML(doc)
-        elif isinstance(doc, etree._Element):
+        else:
             etree_obj = doc
-         
         # return result
         return etree_obj.xpath(xpath)
-        
+
     def json_crawl(self, doc: str = None, url: str = None, _key: str = None):
         """
-        For crawling and formatting json data. it can receive dde string object returned by Response,
-        or it can get the json data through the 'url'.
+        For crawling and formatting json data. it can receive dde string object returned by 
+        Response, or it can get the json data through the 'url'.
         
-        :param doc:  format json data from string. if not empty, the 'doc' param will be processed
-                     first and the value of 'url' param will be ignored. Default=None
-        :param url:  the url address for crawling json data. the address will be searched only when
-                     the 'doc' param is None. Default=None
-        :param _key: after formatting json data, the '_key' param will be use when just need one 
-                     key-value pairs data of json. Default=None
+        ARGS:
+
+        param doc  : format json data from string. if not empty, the 'doc' param will be processed
+                   : first and the value of 'url' param will be ignored. Default=None
+        param url  : the url address for crawling json data. the address will be searched only when
+                   : the 'doc' param is None. Default=None
+        param _key : after formatting json data, the '_key' param will be use when just need one
+                   : key-value pairs data of json. Default=None
+
         """
-        
+        json_result = None
         if doc is not None:
             json_result = json.loads(doc)
         elif url is not None:
@@ -129,20 +149,24 @@ class Crawl():
             return json_result[_key]
         return json_result
 
-    def save(contents, file_prefix: str = 'saved',file_suffix: str = '.jpg',file_dir: str = './',\
+    @staticmethod
+    def save(contents, file_prefix: str = 'saved', file_suffix: str = '.jpg', file_dir: str = './',
             mode: str = 'wb', is_url: bool = False):
         """
         Store the obtained content in a file.
+
+        ARGS:
         
-        :param contents    : the content stored in the file, or the URL to get the content, used with
-                            'is_url' param
-        :param file_prefix : stroed filename prefix. Default=saved
-        :param file_suffix : stored filename suffix. Default=.jpg
-        :param file_dir    : the folder path where the file is stored. Default=./
-        :param mode        : the mode in which the file is stored
-        :param is_url      : determines whether the 'contents' param is a URL. Default=False
+        param contents     : the content stored in the file, or the URL to get the content, used with
+                           : 'is_url' param
+        param file_prefix  : stored filename prefix. Default=saved
+        param file_suffix  : stored filename suffix. Default=.jpg
+        param file_dir     : the folder path where the file is stored. Default=./
+        param mode         : the mode in which the file is stored
+        param is_url       : determines whether the 'contents' param is a URL. Default=False
+
         """
-        
+
         assert contents is not None, 'param \'contents\' should not be None.'
         if is_url:
             pass
@@ -150,9 +174,17 @@ class Crawl():
             os.mkdir(file_dir)
         file_name = file_prefix + file_suffix
         file_path = file_dir + file_name
-        
+
         # write file
         with open(file_path, mode) as file_handle:
             file_handle.write(contents)
-        
+
         print('saving file: {} to {} successfully.'.format(file_name, file_path))
+
+    def __call__(self) -> Any:
+        """
+        when the object of class Crawl is used as a function, this inner function will be use.
+
+        """
+        print("using the inner function \'__call__()\'")
+        pass
