99590ba0cd2317731d3c90a02988593bccc8f3f0
ccg1024 <aimerchenyuan@163.com>
2022-06-22 14:36:37 +0800

    add new crawl file

--- 2048_crawl.py
+++ 2048_crawl.py
@@ -8,6 +8,7 @@ crawl magnet link according to search keyword.
 """
 import spider
 from requests import Response
+from lxml import etree
 
 
 PAGE_URL = 'https://bbs.cp500111.com/2048/thread.php?fid-28-page-{}.html'
@@ -24,7 +25,17 @@ def search_info(url: str = None, keywords: str = None) -> Response:
     assert url is not None, "Maybe need to put a url."
     crawler = spider.Crawl()
     html_content = crawler.get(url).content
-    print(html_content)
+    table_html = crawler._xpath(html_content, '//div[@class="t z"]/table//tbody')
+    tr_xpath_list = crawler._xpath(table_html[0], 'tr')
+    for tr_xpath in tr_xpath_list[8:-2]:
+        # print(etree.tostring(tr_xpath))
+        link_tag = crawler._xpath(tr_xpath, 'td[2]/a')
+        if len(link_tag) > 0:
+            link_tag = link_tag[0]
+            print(crawler._xpath(link_tag, "text()"))
+            print(crawler._xpath(link_tag, "@href"))
+        else:
+            print("[E] Got empty xpath")
 
 
 if __name__ == "__main__":
